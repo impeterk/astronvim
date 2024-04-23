477cce302bc604ce87d6638473dda11397a430c3
Peter Kudelas <contact@peterkudelas.eu>
2024-04-23 16:36:01 +0200

    community plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,14 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.pack.astro" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.svelte" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.vue" },
+  { import = "astrocommunity.editing-support.todo-comments-nvim" },
+  { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.colorscheme.mini-base16", enabled = true },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
